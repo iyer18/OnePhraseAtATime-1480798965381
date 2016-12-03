d2bfd8840c9a1a7cac8a753696cf69c005a8253e
Gunjan Gauri <ggauri@purdue.edu>
2016-12-03 17:04:05 -0500

    setup .js file to set the environment variables of  the app

--- app.js
+++ app.js
@@ -8,8 +8,12 @@
 // for more info, see: http://expressjs.com
 var express = require('express');
 var mongoose = require('mongoose');
+var logger = require('morgan'); // log requests to the console (express4)
 //var mongoURL = 'mongodb://2ee767cd-438d-4b4e-a641-e50187d425e3:534deb6a-c8b2-477d-9b20-3190aa78c9ea@23.246.199.101:10079/db';
 var mongoURL = 'localhost/db';
+var database = require('.config/database');
+var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 mongoose.connect(mongoURL);
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
